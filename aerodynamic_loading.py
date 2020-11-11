3a3d3d79c236996bafe6c70618f7d98860ba3461
BHGDeul <74308924+BHGDeul@users.noreply.github.com>
2020-11-11 15:54:28 +0100

    Update aerodynamic_loading.py

--- aerodynamic_loading.py
+++ aerodynamic_loading.py
@@ -3,7 +3,12 @@ import numpy as np
 data = np.genfromtxt("MainWing_a=0.00_v=10.00ms.txt",skip_header=40,skip_footer=1030)
 
 ylst = data[:,0]
+chordlst = data[:,1]
 Cllst = data[:,3]
 Cdlst = data[:,5]
 Cmlst = data[:,7]
+
+aero_lst = (Cllst,Cdlst,Cmlst)
+
+f_Cllst = sp.interpolate.interp1d(ylst,function,kind='cubic',fill_value='extrapolate')
 
