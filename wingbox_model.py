ca24a31fac231266ad39da7eb5c0c92c792ee7c5
BHGDeul <74308924+BHGDeul@users.noreply.github.com>
2020-11-18 17:07:15 +0100

    Update wingbox_model.py

--- wingbox_model.py
+++ wingbox_model.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from math import sqrt
+from math import sqrt, sin, atan, cos
 import scipy as sp
 from scipy import interpolate
 
@@ -9,6 +9,7 @@ x = data[:,0]
 y = data[:,1]
 
 spar_location = [0.2,0.7]
+t = 0.001
 
 x_values = spar_location
 y_1 = [-0.0633,-0.0304]
@@ -17,7 +18,7 @@ y_2 = [0.0633,0.0537]
 b = (y_2[0]-y_1[0])
 a = (y_2[1]-y_1[1])
 c = y_2[0] - y_2[1]
-
+h = x_values[1]-x_values[0]
 
 
 # numerical integration
@@ -27,9 +28,13 @@ lower = sp.interpolate.interp1d(x_values, y_1, kind='linear',fill_value='extrapo
 lng_upper = sqrt((x_values[0]-x_values[1])**2 + (y_2[0]-y_2[1])**2)
 lng_lower = sqrt((x_values[0]-x_values[1])**2 + (y_1[0]-y_1[1])**2)
 
-dA = 0.0001
-dx = 0.01
-t = dx
+beta_upper = atan((y_2[0]-y_2[1])/0.5)
+beta_lower = atan((-y_1[0]+y_1[1])/0.5)
+
+
+dx = t
+dA = dx**2
+
 
 xs = np.arange(0.2,0.701,dx)
 
@@ -53,15 +58,21 @@ y_bar = (sum_Q + b * t * 0 + a * t * a/2)/(count*dA + (b+a)*t)
 
 x_bar = 0.2 + ((lng_upper + lng_lower)*t * 0.25 + 0.5 * a * t) / ((b+a+lng_lower+lng_upper)*t)
 
-plt.plot(x,y)
-plt.vlines(0.2,-0.0633,0.0633,color='r')
-plt.vlines(0.7,-0.0304,0.053700,color='r')
-plt.hlines(y_bar,0,1)
-plt.vlines(x_bar,lower(x_bar),upper(x_bar))
-plt.plot(x_values,y_1,color='r')
-plt.plot(x_values,y_2,color='r')
-plt.plot(x_bar,y_bar,'rp',markersize=6)
-plt.show()
+
+
+I_polar = 4 * ((a+b)/2 * h)**2 / ((a+b+lng_upper+lng_lower)/t)
+
+## Uncomment to plot the cross section of the wing box ##
+
+##plt.plot(x,y)
+##plt.vlines(0.2,-0.0633,0.0633,color='r')
+##plt.vlines(0.7,-0.0304,0.053700,color='r')
+##plt.hlines(y_bar,0,1)
+##plt.vlines(x_bar,lower(x_bar),upper(x_bar))
+##plt.plot(x_values,y_1,color='r')
+##plt.plot(x_values,y_2,color='r')
+##plt.plot(x_bar,y_bar,'rp',markersize=6)
+###plt.show()
 
 
 
