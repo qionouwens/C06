53c977a4b9eb9d7df6a59cd082f6de70a7247ce8
BHGDeul <74308924+BHGDeul@users.noreply.github.com>
2020-11-18 16:54:57 +0100

    Update wingbox_spanwise.py

--- wingbox_spanwise.py
+++ wingbox_spanwise.py
@@ -1,6 +1,7 @@
 import numpy as np
 from aerodynamic_loading import f_chord
-from wingbox_model import a as aft,b as front, I_xx, I_yy
+from wingbox_model import a as aft,b as front,I_polar, lng_upper, lng_lower
+from moment_of_inertia_wingbox import I_x,I_yy
 
 dihedral = 5#deg
 cr = 9.555 #m
@@ -18,9 +19,15 @@ def front_spar(y):
 def aft_spar(y):
     return aft * f_chord(y)
 
-def I_x(y):
-    return I_xx * f_chord(y)**3
+def upper(y):
+    return lng_upper * f_chord(y)
+
+def lower(y):
+    return lng_lower * f_chord(y)
 
 def I_y(y):
     return I_yy * f_chord(y)**3
+
+def J(y):
+    return I_polar * f_chord(y)**3
 
