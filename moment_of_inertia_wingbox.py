5a7cd3620d3670e1c1776cf9337f798b0e59d8bf
BHGDeul <74308924+BHGDeul@users.noreply.github.com>
2020-11-20 10:11:48 +0100

    Update moment_of_inertia_wingbox.py

--- moment_of_inertia_wingbox.py
+++ moment_of_inertia_wingbox.py
@@ -1,19 +1,21 @@
-from wingbox_model import x_values, y_1, y_2, h, t, a, b, x_bar, y_bar, spar_location, lng_upper,lng_lower, upper, lower, beta_upper, beta_lower
+from wingbox_model import x_values, y_2, h, t_skin, t_spar, a, b, x_bar, y_bar, lng_upper,lng_lower, upper, lower, beta_upper, beta_lower
 from aerodynamic_loading import f_chord
-from math import sin, cos
+from math import sin
 import numpy as np
-import pdb
 
+
+
+height_stringer = 20 #1/skin_thickness
 
 ### moment of inertia computations
-A = 1/12 * t * b**3 + (b*t)*((y_2[0] - b/2)-y_bar)**2 
-B = 1/12 * t * a**3 + (a*t)*((y_2[1] - a/2)-y_bar)**2
-C = (t * lng_upper**3 * (sin(beta_upper)**2)) / 12 + (lng_upper)*t * (upper(0.45)-y_bar)**2
-D = (t * lng_lower**3 * (sin(beta_lower)**2)) / 12 + (lng_lower)*t * (lower(0.45)-y_bar)**2
+A = 1/12 * t_spar * b**3 + (b*t_spar)*((y_2[0] - b/2)-y_bar)**2 
+B = 1/12 * t_spar * a**3 + (a*t_spar)*((y_2[1] - a/2)-y_bar)**2
+C = (t_skin * lng_upper**3 * (sin(beta_upper)**2)) / 12 + (lng_upper)*t_skin * (upper(0.45)-y_bar)**2
+D = (t_skin * lng_lower**3 * (sin(beta_lower)**2)) / 12 + (lng_lower)*t_skin * (lower(0.45)-y_bar)**2
 
 
 ## stringer placement upper skin##
-stringer_area = (2*t)**2 * 20
+stringer_area = (2*t_skin)**2 * height_stringer
 
 def I_x(y,n_stringers_upper,n_stringers_lower):
     skins = [[upper,n_stringers_upper],[lower,n_stringers_lower]]
@@ -26,11 +28,11 @@ def I_x(y,n_stringers_upper,n_stringers_lower):
         
     return (A+B+C+D)*f_chord(y)**3 +I_induced
 
-E = (b*t)*(spar_location[1]-x_bar)**2
-F = (b*t)*(spar_location[0]-x_bar)**2
-G = (t * lng_upper**3 * (cos(beta_upper)**2)) / 12 + (lng_upper)*t * ((spar_location[1]-spar_location[0])-x_bar)**2
-H = (t * lng_lower**3 * (cos(beta_lower)**2)) / 12 + (lng_lower)*t * ((spar_location[1]-spar_location[0])-x_bar)**2
+# E = (b*t)*(spar_location[1]-x_bar)**2
+# F = (b*t)*(spar_location[0]-x_bar)**2
+# G = (t * lng_upper**3 * (cos(beta_upper)**2)) / 12 + (lng_upper)*t * ((spar_location[1]-spar_location[0])-x_bar)**2
+# H = (t * lng_lower**3 * (cos(beta_lower)**2)) / 12 + (lng_lower)*t * ((spar_location[1]-spar_location[0])-x_bar)**2
 
-I_yy = (E+F+G+H)
+# I_yy = (E+F+G+H)
 
 
