b40b5a719e0e9255ed9848731f13685e74a6ea42
qionouwens <65192564+qionouwens@users.noreply.github.com>
2020-11-20 10:12:01 +0100

    Update Shear_Force_diagrams.py

--- Shear_Force_diagrams.py
+++ Shear_Force_diagrams.py
@@ -2,46 +2,46 @@ from aerodynamic_loading import force_distribution, angle_of_attack
 from math import cos, sin, radians
 import numpy as np
 import scipy
-import thread
 from scipy import integrate
 import matplotlib.pyplot as plt
 
 half_span = 31.315
-desired_cl = 0.5
+desired_cl = 1.5
 aoa = angle_of_attack(desired_cl)
 
 
 def normal_force(y):
-    Sin =-767926.5287+34501.01*y-318.645*y**2
-    Sin2 = -15320*(y-9.44)
-    if 9.44 <= y <= 12.49:
-        Sadd = Sin +Sin2
-    else:
-        Sadd = Sin
-    lift, drag, moment = force_distribution(y, desired_cl)
-    return lift*cos(radians(aoa))+sin(radians(aoa))*drag+Sadd
+    lift, drag, _ = force_distribution(y, desired_cl)
+    return lift*cos(radians(aoa))+sin(radians(aoa))*drag
 
 
 def shear_force(y):
-    return scipy.integrate.quad(normal_force, y, half_span)[0]
+    add = 0
+    Sin = -767926.5287 + 34501.01 * y - 318.645 * y ** 2
+    if y <= 10.7:
+        add = -71259.84
+    return scipy.integrate.quad(normal_force, y, half_span)[0]+add+Sin
 
 
 def moment(y):
     return scipy.integrate.quad(shear_force, y, half_span)[0]
 
 
-y_values = np.linspace(0, half_span, 200)
+y_values = np.linspace(0, half_span, 2000)
 shear = []
 moment_list = []
+previous_shear = shear_force(0)
+previous_y_value = 0
 for y_value in y_values:
     shear.append(shear_force(y_value))
-    moment_list.append(moment(y_value))
+moment_list = scipy.integrate.cumtrapz(shear, initial=moment(0))
+moment_list = max(moment_list) - moment_list
 
-fig, ax = plt.subplots()
-ax.plot(y_values, shear)
-plt.show()
+plt.figure()
+plt.subplot(211)
+plt.plot(y_values, shear)
 
-fig, ax = plt.subplots()
-ax.plot(y_values, Mbend)
+plt.subplot(212)
+plt.plot(y_values, moment_list)
 plt.show()
 
